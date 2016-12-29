398f1adbd3e4ac3ede106a81e9bc7530c38c8e62
LightProger <photoluxe@mail.ru>
2016-12-29 21:29:39 +0600

    Конвертер температур

--- src/Temperatureconverteralternative.java
+++ src/Temperatureconverteralternative.java
@@ -6,17 +6,17 @@ public class Temperatureconverteralternative {
     public static void main(String[] args) {
         System.out.println("Конвертер температур.");
         Temperatureconverteralternative tca = new Temperatureconverteralternative();
-        tca.convertTemp(2850, 'g');
+        tca.convertTemp(36.6, 'c');
 
     }
 
-    public void convertTemp (int temperature, char convertTo){
+    public void convertTemp (double temperature, char convertTo){
 
         if(convertTo == 'f'){
-          temperature = temperature * 50;
+          temperature = temperature * 1.8 +32;
             System.out.println("Ваша температура в Фаренгейтах равна: " + temperature + "F.");
         } else if(convertTo == 'c'){
-             temperature = temperature / 50;
+             temperature = (temperature - 32) * 5 / 9;
             System.out.println("Ваша температура в Цельсиях равна: " + temperature + "С.");
         } else{
             System.out.println("Ошибка! Введите f или с");
